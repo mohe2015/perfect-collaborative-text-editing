dfbea4b60bd8154cf378f34f458c5bc2d2a7febb
Moritz Hedtke <moritz.hedtke@stud.tu-darmstadt.de>
2024-08-11 19:12:31 +0200

    wip

--- src/pcte.rs
+++ src/pcte.rs
@@ -6,10 +6,50 @@ pub struct Pcte {
 }
 
 pub struct PcteNode {
-    character: char,
+    pub character: Option<char>,
 }
 
 pub struct PcteTreeNode {
     pub node: PcteNode,
-    pub children: Vec<PcteTreeNode>
+    pub children: Vec<PcteTreeNode>,
+}
+
+impl Pcte {
+    pub fn new() -> Self {
+        let left_root_node = PcteNode { character: None };
+        let right_root_node = PcteNode { character: None };
+        Self {
+            left_origin_tree: PcteTreeNode {
+                node: left_root_node,
+                children: Vec::new(),
+            },
+            right_origin_tree: PcteTreeNode {
+                node: right_root_node,
+                children: Vec::new(),
+            },
+        }
+    }
+
+    pub fn insert(&mut self, index: usize, node: PcteNode) {}
+}
+
+impl PcteTreeNode {
+    pub fn node_first_node_at_index(&self, mut index: usize) -> Result<&PcteTreeNode, usize> {
+        if (index == 0) {
+            Ok(self)
+        } else {
+            index -= 1;
+            for child in &self.children {
+                match child.node_first_node_at_index(index) {
+                    Ok(ok) => return Ok(ok),
+                    Err(new_index) => {
+                        index = new_index;
+                    }
+                };
+            }
+            Err(index)
+        }
+    }
+
+    pub fn node_last_node_at_index(&self, index: usize) -> &PcteTreeNode {}
 }
